f6eb66f787b1151ff081b8ce241070cecc5018ec
Davi Hansen <salmao.pedra@gmail.com>
2025-01-15 12:33:20 -0300

    Camara v2

--- docs/views.py
+++ docs/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
-
 from docs.models import Docs 
+from django.db.models import Q
 
 
 
 def docs_view(request):
-
-     docs = Docs.objects.all()# pega todos os objetos do banco de dados all
-     
+     docs =Docs.objects.all().order_by('tipe')
      search = request.GET.get('search')
-
-     if search:
-          docs = Docs.object.filter(tipe__icontains =search)
+     tipos = request.GET.get('tipos')
      
+
+     if not tipos:
+          docs = Docs.objects.filter(Q(autor__icontains=search)|Q(tipe__name__icontains=tipos))
+     
+     if search and tipos:
+      docs = Docs.objects.filter(autor__icontains = search, tipe__name__icontains = tipos)
+     
+
      return render(
           request ,
          'docs.html', 
-         {'docs': docs})#pega o arquivo HTML da pasta templates ejoga para o usuario 
+         {'docs': docs})#pega o arquivo HTML da pasta templates e joga para o usuario 
  
